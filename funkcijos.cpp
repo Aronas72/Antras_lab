bcf7de947ede342aa5b544f8f2cacfb79645f6be
Aronas72 <aronas.bagdevicius@mif.stud.vu.lt>
2025-11-19 23:10:51 +0200

    Update funkcijos.cpp

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -39,24 +39,35 @@ void strategija2_list(list<Studentas>& grupe, list<Studentas>& vargsiukai, int p
         else ++it;}}
 
 void isvedimas(const list<Studentas>& vargsiukai, const list<Studentas>& galvociai, int rik, int balas, bool i_ekrana) {
-    list<Studentas> v=vargsiukai;
-    list<Studentas> g=galvociai;
+    list<Studentas> v = vargsiukai;
+    list<Studentas> g = galvociai;
+
     bool (*lyg)(const Studentas&, const Studentas&);
-    if (rik==1) {
-        lyg=compare;}
-    else if (rik==2) {
-        lyg=comparePagalPavarde;}
-    else if(rik==3){
-        if (balas==1){
-            lyg=comparePagalVid;}
-    else{
-        lyg = comparePagalMed;}}
+    if (rik == 1) lyg = compare;
+    else if (rik == 2) lyg = comparePagalPavarde;
+    else if (rik == 3) {
+        if (balas == 1) lyg = comparePagalVid;
+        else lyg = comparePagalMed;}
 
     v.sort(lyg);
     g.sort(lyg);
-    auto spausdinti_antrašte = [](std::ostream& os) {
-        os << setw(15) << left << "Vardas" << setw(20) << left << "Pavarde"<< setw(5) << right << "ND1"<< setw(5) << right << "ND2"<< setw(5) << right << "ND3"<< setw(5) << right << "ND4"<< setw(5) << right << "ND5"<< setw(8) << right << "Egz."<< setw(18) << right << "Galutinis (Med.)"<< setw(18) << right << "Galutinis (Vid.)" << endl;
-        os << string(104, '-') << endl;};
+
+    auto spausdinti_antraste = [&](std::ostream& os) {
+        os << setw(15) << left << "Vardas" << setw(20) << left << "Pavarde";
+        if (balas == 1) os << setw(18) << right << "Galutinis (Vid.)";
+        else if (balas == 2) os << setw(18) << right << "Galutinis (Med.)";
+        else os << setw(18) << right << "Galutinis (Med.)" << setw(18) << right << "Galutinis (Vid.)";
+        os << endl;
+        os << string(53 + (balas == 3 ? 18 : 0), '-') << endl;};
+
+    auto spausdinti_studentus = [&](std::ostream& os, const list<Studentas>& l) {
+        for (const auto& st : l) {
+            os << setw(15) << left << st.vardas() << setw(20) << left << st.pavarde();
+            if (balas == 1) os << setw(18) << right << fixed << setprecision(2) << st.gal;
+            else if (balas == 2) os << setw(18) << right << fixed << setprecision(2) << st.med;
+            else {
+                os << setw(18) << right << fixed << setprecision(2) << st.med << setw(18) << right << fixed << setprecision(2) << st.gal;}
+            os << endl;}};
 
     if (!i_ekrana) {
         auto isvesti_i_faila = [&](const string& pav, const list<Studentas>& studentai) {
@@ -64,23 +75,22 @@ void isvedimas(const list<Studentas>& vargsiukai, const list<Studentas>& galvoci
             if (!fo.is_open()) {
                 cout << "Klaida: Nepavyko atidaryti failo " << pav << endl;
                 return;}
-            spausdinti_antrašte(fo);
-            for(const auto& st : studentai) {
-                fo << st;}
+            spausdinti_antraste(fo);
+            spausdinti_studentus(fo, studentai);
             fo.close();};
         isvesti_i_faila("vargsiukai.txt", v);
         isvesti_i_faila("galvociai.txt", g);
         cout << "Duomenys surikiuoti ir irasyti i vargsiukai.txt ir galvociai.txt failus." << endl;}
-        else {
-            cout << "\n--- GALVOCIAI ---" << endl;
-            spausdinti_antrašte(cout);
-            for(const auto& st : g) {
-                cout << st;}
-            cout << "\n--- VARGSIUKAI ---" << endl;
-            spausdinti_antrašte(cout);
-            for(const auto& st : v) {
-                cout << st;}}
+    else {
+        cout << "\n--- GALVOCIAI ---" << endl;
+        spausdinti_antraste(cout);
+        spausdinti_studentus(cout, g);
+        cout << "\n--- VARGSIUKAI ---" << endl;
+        spausdinti_antraste(cout);
+        spausdinti_studentus(cout, v);}
+
     cout << "Is viso: " << vargsiukai.size() << " vargsiuku ir " << galvociai.size() << " galvociu." << endl;}
+
 
 void testavimas(){
     string failai[]={"mano100000.txt", "mano1000000.txt"};
