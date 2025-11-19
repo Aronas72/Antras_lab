ec1cbcf825e434bf953049586a2b7e99b6b14850
Aronas72 <aronas.bagdevicius@mif.stud.vu.lt>
2025-11-19 21:36:49 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -5,17 +5,15 @@
 #include <random>
 #include <chrono>
 #include <iomanip>
+#include <fstream>
 
 using std::mt19937;
 using std::string;
 using std::ifstream;
 using std::ofstream;
-using std::setw;
-using std::left;
-using std::right;
 
-list<Studentas> nuskaitymas(const string& failpav);
-void isvedimas(const list<Studentas>& vargsiukai, const list<Studentas>& galvociai, int rik, int balas);
+list<Studentas> nuskaitymas(const string& saltinis, bool is_failo);
+void isvedimas(const list<Studentas>& vargsiukai, const list<Studentas>& galvociai, int rik, int balas, bool i_ekrana);
 void strategija2_list(list<Studentas>& grupe, list<Studentas>& vargsiukai, int pasirinkti);
 void testavimas();
 
