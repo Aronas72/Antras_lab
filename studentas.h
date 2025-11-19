92c87f69bd8b96e01b5b53084d62663194b4b5e7
Aronas72 <aronas.bagdevicius@mif.stud.vu.lt>
2025-11-19 23:12:02 +0200

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -6,10 +6,10 @@
 #include <vector>
 #include <list>
 #include <algorithm>
-#include <cmath>
 #include <iomanip>
 #include <sstream>
 #include <limits>
+#include <random>
 
 using std::string;
 using std::vector;
@@ -27,43 +27,45 @@ using std::right;
 using std::sort;
 using std::ios_base;
 
-double mediana(const vector<double>& v);
-double vidurkis(const vector<double>& v);
+double mediana(const vector<int>& v);
+double vidurkis(const vector<int>& v);
 
 class Studentas {
 private:
     string vardas_;
     string pavarde_;
-    double egzaminas_;
-    vector<double> nd_;
+    int egzaminas_;           // Pakeista į int
+    vector<int> nd_;          // Pakeista į int
 
 public:
     double med;
     double gal;
-    
-    Studentas() : egzaminas_(0.0), med(0.0), gal(0.0) {}
+
+    Studentas() : egzaminas_(0), med(0.0), gal(0.0) {}
     Studentas(istream& is);
 
     ~Studentas() {
-    vardas_.clear();
-    pavarde_.clear();
-    egzaminas_=0;
-    nd_.clear();}
-    
+        vardas_.clear();
+        pavarde_.clear();
+        egzaminas_=0;
+        nd_.clear();
+    }
+
     Studentas(const Studentas& other);
     Studentas& operator=(const Studentas& other);
-    
+
     inline string vardas() const { return vardas_; }
     inline string pavarde() const { return pavarde_; }
-    inline double egzaminas() const { return egzaminas_; }
-    inline const vector<double>& nd() const { return nd_; }
+    inline int egzaminas() const { return egzaminas_; }
+    inline const vector<int>& nd() const { return nd_; }
 
-    double galBalas(double (*nd_skaic)(const vector<double>&) = mediana) const;
+    double galBalas(double (*nd_skaic)(const vector<int>&) = mediana) const;
 
     istream& readStudent(istream& is);
-    
+
     friend istream& operator>>(istream& is, Studentas& studentas);
-    friend ostream& operator<<(ostream& os, const Studentas& studentas);};
+    friend ostream& operator<<(ostream& os, const Studentas& studentas);
+};
 
 ostream& operator<<(ostream& os, const Studentas& studentas);
 istream& operator>>(istream& is, Studentas& studentas);
