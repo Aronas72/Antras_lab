10469bc5ea29427c40ca4330ec02fccbc30a17a1
Aronas72 <aronas.bagdevicius@mif.stud.vu.lt>
2025-11-26 18:51:10 +0200

    Update studentas.cpp

--- studentas.cpp
+++ studentas.cpp
@@ -1,26 +1,20 @@
 #include "studentas.h"
 #include <random>
 #include <cmath>
+#include <iostream>
 
 double vidurkis(const vector<int>& v) {
     if (v.empty()) return 0.0;
     double sum = 0.0;
-    for (int pazymys : v) sum += pazymys;
+    for (int p : v) sum += p;
     return sum / v.size();}
 
 double mediana(const vector<int>& v) {
     if (v.empty()) return 0.0;
     vector<int> temp = v;
-    sort(temp.begin(), temp.end());
-    size_t x = temp.size();
-    if (x % 2 == 0) return (temp[x / 2 - 1] + temp[x / 2]) / 2.0;
-    else return temp[x / 2];}
-
-Studentas::Studentas(istream& is) {
-    is >> *this;}
-
-double Studentas::galBalas(double (*nd_skaic)(const vector<int>&)) const {
-    return nd_skaic(nd_)*0.4+egzaminas_*0.6;}
+    std::sort(temp.begin(), temp.end());
+    size_t n = temp.size();
+    return (n % 2 == 0) ? (temp[n/2-1]+temp[n/2])/2.0 : temp[n/2];}
 
 Studentas::Studentas(const Studentas& other) :
     vardas_(other.vardas_),
@@ -38,102 +32,82 @@ Studentas& Studentas::operator=(const Studentas& other) {
         nd_ = other.nd_;
         med = other.med;
         gal = other.gal;}
+    
     return *this;}
 
-istream& Studentas::readStudent(istream& is) {
+double Studentas::galBalas(double (*nd_skaic)(const vector<int>&)) const {
+    return nd_skaic(nd_)*0.4 + egzaminas_*0.6;}
+
+
+istream& operator>>(istream& is, Studentas& s) {
+    s.nd_.clear();
+
     if (&is == &std::cin) {
-        cout << "Iveskite varda: ";
-        if (!(is >> vardas_)) return is;
+        std::cout << "Iveskite varda: ";
+        if (!(is >> s.vardas_)) return is;
 
-        cout << "Iveskite pavarde: ";
-        if (!(is >> pavarde_)) return is;
+        std::cout << "Iveskite pavarde: ";
+        if (!(is >> s.pavarde_)) return is;
 
-        cout << "Ar norite sugeneruoti atsitiktinius ND ir egzamino pazymius? (1 - taip, 0 - ne): ";
-        int ats;
+        std::cout << "Ar generuoti atsitiktinius ND ir egzamino balus? (1-taip,0-ne): ";
+        int ats = 0;
         if (!(is >> ats)) ats = 0;
 
-        nd_.clear();
         if (ats == 1) {
             std::random_device rd;
             std::mt19937 gen(rd());
-            std::uniform_int_distribution<int> dist(1, 10);
+            std::uniform_int_distribution<int> dist(1,10);
 
             int nd_kiek = 5;
-            cout << "Kiek ND sugeneruoti? (default 5): ";
+            std::cout << "Kiek ND sugeneruoti? ";
             if (!(is >> nd_kiek) || nd_kiek <= 0) nd_kiek = 5;
 
-            for (int i = 0; i < nd_kiek; ++i)
-                nd_.push_back(dist(gen));
+            for(int i=0;i<nd_kiek;i++) s.nd_.push_back(dist(gen));
+            s.egzaminas_ = dist(gen);
 
-            egzaminas_ = dist(gen);
-
-            cout << "Sugeneruoti ND: ";
-            for (int d : nd_) cout << d << " ";
-            cout << "\nSugeneruotas egzamino balas: " << egzaminas_ << endl;}
+            std::cout << "Sugeneruoti ND: ";
+            for(auto d : s.nd_) std::cout << d << " ";
+            std::cout << "Egzaminas: " << s.egzaminas_ << std::endl;}
         else {
-            int k;
-            cout << "Iveskite namu darbu pazymius (1-10, pabaigai ivesti ne skaiciu): " << endl;
-            while (cout << "ND pazymys: " && (is >> k) && k >= 1 && k <= 10) {
-                nd_.push_back(k);}
+            std::cout << "Iveskite ND (1-10, pabaigai - ne skaiciu):" << std::endl;
+            int paz;
+            while(std::cout<<"ND: " && (is>>paz) && paz>=1 && paz<=10) s.nd_.push_back(paz);
+
             is.clear();
-            is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            is.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
 
-            cout << "Iveskite egzamino bala (1-10): ";
-            int egz_balas = 0;
-            if (!(is >> egz_balas) || egz_balas < 1 || egz_balas > 10) {
-                cout << "Neteisinga egzamino balu ivestis. Nustatoma 0." << endl;
-                egzaminas_ = 0;
-                is.clear();
-                is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                is.setstate(ios_base::failbit);
-                return is;}
-            egzaminas_ = egz_balas;}}
-    else {
-        string eilute;
-        if (std::getline(is, eilute)) {
-            std::istringstream iss(eilute);
-            if (!(iss>>vardas_>>pavarde_)) return is;
+            std::cout<<"Egzaminas (1-10): ";
+            if(!(is>>s.egzaminas_) || s.egzaminas_<1 || s.egzaminas_>10){
+                s.egzaminas_=0;
+                is.setstate(std::ios_base::failbit);
+                return is;}}}
+    else{
+        string eil;
+        if(!std::getline(is,eil)){ is.setstate(std::ios_base::failbit); return is; }
+        std::istringstream iss(eil);
 
-            nd_.clear();
-            vector<int> pazymiai;
-            int k;
-            while (iss>>k){
-                pazymiai.push_back(k);}
-            if (pazymiai.empty()){
-                egzaminas_=0;}
-            else{
-                egzaminas_=pazymiai.back();
-                pazymiai.pop_back();
-                nd_=pazymiai;}}
-        else {
-            is.setstate(ios_base::failbit);
-            return is;}}
+        if(!(iss>>s.vardas_>>s.pavarde_)){ is.setstate(std::ios_base::failbit); return is; }
 
-    double nd_vid = vidurkis(nd_);
-    double nd_med = mediana(nd_);
-    med = nd_med*0.4+egzaminas_*0.6;
-    gal = nd_vid*0.4+egzaminas_*0.6;
+        vector<int> paz;
+        int x;
+        while(iss>>x) paz.push_back(x);
+
+        if(!paz.empty()){ s.egzaminas_=paz.back(); paz.pop_back(); } else s.egzaminas_=0;
+
+        s.nd_ = std::move(paz);}
+
+    double nd_vid = vidurkis(s.nd_);
+    double nd_med = mediana(s.nd_);
+    s.med = nd_med*0.4 + s.egzaminas_*0.6;
+    s.gal = nd_vid*0.4 + s.egzaminas_*0.6;
 
     return is;}
 
-istream& operator>>(istream& is, Studentas& studentas) {
-    return studentas.readStudent(is);}
-
-ostream& operator<<(ostream& os, const Studentas& studentas) {
-    os << setw(15) << left << studentas.vardas()
-       << setw(20) << left << studentas.pavarde()
-       << setw(18) << right << fixed << setprecision(2) << studentas.med
-       << setw(18) << right << fixed << setprecision(2) << studentas.gal << endl;
+ostream& operator<<(ostream& os, const Studentas& s) {
+    os << std::setw(15) << std::left << s.vardas_ << std::setw(20) << std::left << s.pavarde_ << std::setw(18) << std::right << std::fixed << std::setprecision(2) << s.med << std::setw(18) << std::right << std::fixed << std::setprecision(2) << s.gal << std::endl;
     return os;}
 
-bool compare(const Studentas& a, const Studentas& b) {
-    return a.vardas()<b.vardas();}
-
-bool comparePagalPavarde(const Studentas& a, const Studentas& b) {
-    return a.pavarde()<b.pavarde();}
-
-bool comparePagalMed(const Studentas& a, const Studentas& b) {
-    return a.med < b.med;}
-
-bool comparePagalVid(const Studentas& a, const Studentas& b) {
-    return a.gal < b.gal;}
+bool compare(const Studentas& a, const Studentas& b){ return a.vardas() < b.vardas();}
+bool comparePagalPavarde(const Studentas& a, const Studentas& b){ return a.pavarde() < b.pavarde();}
+bool comparePagalMed(const Studentas& a, const Studentas& b){ return a.med < b.med;}
+bool comparePagalVid(const Studentas& a, const Studentas& b){ return a.gal < b.gal;}
