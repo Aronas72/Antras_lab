3cb46ecc4aa471b6e38f9b67f4b94d8df20db785
Aronas72 <aronas.bagdevicius@mif.stud.vu.lt>
2025-12-02 18:45:00 +0200

    sukurta doxygen dokumentacija

---
+++ doxygen/class_studentas.js
@@ -0,0 +1,13 @@
+var class_studentas =
+[
+    [ "Studentas", "class_studentas.html#ab459e995e8c9b24cdc9aec5b09a66539", null ],
+    [ "~Studentas", "class_studentas.html#a63e449e0f51a0b14340d02ee71b4be23", null ],
+    [ "Studentas", "class_studentas.html#aef0484fe46cf05746f8ffc0d083fcf3e", null ],
+    [ "egzaminas", "class_studentas.html#a8ec75244e6c21cf5a6af9f6d02f20fa2", null ],
+    [ "nd", "class_studentas.html#a6b0c1673eae4c918c68be80d132af74d", null ],
+    [ "operator=", "class_studentas.html#a25bcc531503d3bc490dab4a656e3b8e9", null ],
+    [ "egzaminas_", "class_studentas.html#ab2b6d614a1491c86854d0cd8454a4926", null ],
+    [ "gal", "class_studentas.html#a2645ab322ba0641fc7e9381c6a2e1c0a", null ],
+    [ "med", "class_studentas.html#a1cf27118e7d06102a22c47adaa1cf624", null ],
+    [ "nd_", "class_studentas.html#a0b9d02ebc076cf0a749720fcc8dae568", null ]
+];
---
+++ doxygen/class_zmogus.js
@@ -0,0 +1,13 @@
+var class_zmogus =
+[
+    [ "Zmogus", "class_zmogus.html#a905fef3b8ad421a6d023c7ebc295f48e", null ],
+    [ "Zmogus", "class_zmogus.html#a6ca61a3bcea221478551dd04419d2433", null ],
+    [ "~Zmogus", "class_zmogus.html#a6183937c7da8254430a6c99cfa89fe69", null ],
+    [ "galBalas", "class_zmogus.html#a696aa1034e953581ae3a4c6bfebccfe6", null ],
+    [ "pavarde", "class_zmogus.html#a0ccc9864e6fb821e519fc50afd96dfe1", null ],
+    [ "vardas", "class_zmogus.html#a9ba88c78eedc8094a3b38cdf55228bae", null ],
+    [ "operator<<", "class_zmogus.html#af91f9671d3dbc988ada25cd3a0bf05c5", null ],
+    [ "operator>>", "class_zmogus.html#a4cd9ea7a82145e5facc723c96384da0f", null ],
+    [ "pavarde_", "class_zmogus.html#a95b83ef4d9bbe9b88d78c17563bafa5a", null ],
+    [ "vardas_", "class_zmogus.html#a09b290c9be6039ce3a94e1557def9b3a", null ]
+];
