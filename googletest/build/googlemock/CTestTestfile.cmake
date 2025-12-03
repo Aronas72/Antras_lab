0620ff32e44bcaff98ec3ee7c2bec26671bae69d
Aronas72 <aronas.bagdevicius@mif.stud.vu.lt>
2025-12-03 19:25:41 +0200

    Delete googletest directory

--- googletest/build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aronasbagdevicius/Desktop/v2.0/v2.0/googletest/googlemock
-# Build directory: /Users/aronasbagdevicius/Desktop/v2.0/v2.0/googletest/build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- googletest/build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aronasbagdevicius/Desktop/v2.0/v2.0/googletest/googletest
-# Build directory: /Users/aronasbagdevicius/Desktop/v2.0/v2.0/googletest/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
