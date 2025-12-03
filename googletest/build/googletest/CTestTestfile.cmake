10d6a5f8ca91c8d7d6578a17ff573f638f31ea98
Aronas72 <aronas.bagdevicius@mif.stud.vu.lt>
2025-12-03 18:42:52 +0200

    Add files via upload

---
+++ googletest/build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/aronasbagdevicius/Desktop/v2.0/v2.0/googletest/googletest
+# Build directory: /Users/aronasbagdevicius/Desktop/v2.0/v2.0/googletest/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
